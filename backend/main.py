ca5863d1574cfab4b16d5de2aa8e00e5aaaaa48f
saranshtyagi <18saranshtyagi2004@gmail.com>
2026-07-10 15:29:47 +0530

    Add CORS middleware to FastAPI application for improved cross-origin requests handling

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from app.api.router import api_router
 from app.db.database import Base
 from app.db.database import engine
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
@@ -14,6 +15,16 @@ app = FastAPI(
 
     description="Backend API for AI Meeting Assistant",
 
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(api_router)
